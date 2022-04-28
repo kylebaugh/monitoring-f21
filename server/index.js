533c3a259334d56dc364c20df78b46b468a602d0
kylebaugh <kylebaugh1@gmail.com>
2022-04-28 13:19:12 -0600

    testing

--- server/index.js
+++ server/index.js
@@ -1,12 +1,34 @@
+// const express = require('express')
+// const path = require('path')
+// const Rollbar = require('rollbar')
+
+// let rollbar = new Rollbar({
+//     accessToken: '',
+//     captureUncaught: true,
+//     captureUnhandledRejections: true
+// })
+
+// const app = express()
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../index.html'))
+// })
+
+// const port = process.env.PORT || 4545
+
+// app.listen(port, () => console.log(`Server running on port ${port}`))
+
+
 const express = require('express')
 const path = require('path')
-const Rollbar = require('rollbar')
 
-let rollbar = new Rollbar({
-    accessToken: '',
-    captureUncaught: true,
-    captureUnhandledRejections: true
-})
+// const Rollbar = require('rollbar')
+
+// let rollbar = new Rollbar({
+//     accessToken: '',
+//     captureUncaught: true,
+//     captureUnhandledRejections: true
+// })
 
 const app = express()
 
@@ -14,6 +36,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-const port = process.env.PORT || 4545
+const port = process.env.PORT || 4005
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () =>{
+    console.log(`Listening on port ${port}`)
+})
