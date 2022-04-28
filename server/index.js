c46861391eaa8e638e1dc1a501fa48aebbda08d1
kylebaugh <kylebaugh1@gmail.com>
2022-04-28 13:30:49 -0600

    working?

--- server/index.js
+++ server/index.js
@@ -22,13 +22,16 @@
 const express = require('express')
 const path = require('path')
 
-// const Rollbar = require('rollbar')
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '61a8299df9294656ab54d3e06c247f14',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
-// let rollbar = new Rollbar({
-//     accessToken: '',
-//     captureUncaught: true,
-//     captureUnhandledRejections: true
-// })
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 const app = express()
 
