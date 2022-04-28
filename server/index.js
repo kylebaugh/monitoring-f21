923b1b90be140a64982b5155a8f572b5a8234b69
kylebaugh <kylebaugh1@gmail.com>
2022-04-28 13:08:56 -0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const Rollbar = require('rollbar')
+
+let rollbar = new Rollbar({
+    accessToken: '',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+const port = process.env.PORT || 4545
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
